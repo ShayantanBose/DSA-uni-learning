d4f27face228793c6712312b976e3c1910ac4bfa
Shayantan_Bose <shnbose13@gmail.com>
2024-08-14 19:44:30 +0530

    added an element insert method

--- 1-15-array-question.c
+++ 1-15-array-question.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-void InsertElem(int arr[],int size, int elem){
+
+
+
+void InsertElem(int arr[],int size, int elem, int position){
     printf("Enter the element of the matrix :- ");
     scanf("%d", &elem);
+    if(position==0 || position>size){
+        printf("Invalid Position");
+    }
+    for(int i=size;i>position;i--){
+        arr[i]=arr[size-i];
+    }
+    arr[position]=elem;
+    size++;
 };
 
 void printarray(int arr[],int size){
@@ -13,17 +24,30 @@ void printarray(int arr[],int size){
 int main(){
     int arr[]={1,2,3,4};
     int input;
-    int size;
+    int size,position,elem;
     
-    
+
     printf("Enter the size of the matrix :- ");
     scanf("%d", &size);
     
-    
+   
     for(int i=0;i<=size;i++){
         printf("Enter the elements of the matrix :- \n");
         scanf("%d", &arr[i]);
     }
+
+    printf("Enter Y if you want to add element in the array:- ");
+    scanf("%d", &input);
+
+
+    if(input=='Y'){
+        printf("Enter which position do you want to change in:- ");
+        scanf("%d", &position);
+        printf("Enter the elem you wanted to insert:- ");
+        scanf("%d", &elem);
+        InsertElem(arr,size,elem,position);
+    }
+
 
     printarray(arr,size);
 }
