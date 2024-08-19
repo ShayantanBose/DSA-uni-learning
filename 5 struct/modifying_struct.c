6f693a5cac0a1ec3383dc7c47670f9ef8e71ddd8
Shayantan_Bose <shnbose13@gmail.com>
2024-08-20 00:26:06 +0530

    made huge ass mess

--- 5 struct/modifying_struct.c
+++ 5 struct/modifying_struct.c
@@ -1,12 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_TITLE 100
+
 struct Student{
     int ID;
-    char Name[50];
-    float Percentage;
+    char Name[MAX_TITLE];
+    int Percentage;
 };
+
+// Initialization of the structure
+void InitStruct(struct Student **std, int *capacity, int *count){
+    *count=0;
+    *capacity=2;
+    *std=(struct Student*)malloc(*capacity *sizeof(struct Student));
+    if(*std==NULL){
+        printf("Memory Allocation failed");
+        exit(1);
+    }
+
+};
+
+//Inserting elements in the structure
+void InsertStruct(struct Student **std,int *capacity, int *count, int ID, float *Percentage, char *Name){
+    if(*count==*capacity){
+        *capacity*=2;
+        *std=(struct Student *)realloc(*std, *capacity *sizeof(struct Student));
+        if(*std==NULL){
+            printf("the memory allocation failed");
+            exit(1);
+        }
+        (*std)[*count].ID=ID;
+        strncpy((*std)[*count].Name, Name, MAX_TITLE -1);
+        (*std)[*count].Name[MAX_TITLE-1]='\0';
+        (*std)[*count].Percentage=Percentage;
+    }
+};
+
+void DeleteStruct(struct Student *std,int *count,int ID){
+    
+}
 int main(){
-    struct Student std;
-    struct Student 
+    
     // for(int i=1;i<4;i++){
     //     printf("What is the name of the Student:- ");
     //     scanf("%s",&std[i].Name);
