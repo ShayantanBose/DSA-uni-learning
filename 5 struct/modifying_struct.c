26e53d5f6c426cd95c4e394fa6e5fd66c35315bb
Shayantan_Bose <shnbose13@gmail.com>
2024-08-20 01:39:02 +0530

    changes

--- 5 struct/modifying_struct.c
+++ 5 struct/modifying_struct.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define MAX_TITLE 100
-
 struct Student {
     int ID;
     char Name[MAX_TITLE];
     int Percentage;
 };
-
-// Initialization of the structure
 void InitStruct(struct Student **std, int *capacity, int *count) {
     *count = 0;
     *capacity = 2;
@@ -20,8 +16,6 @@ void InitStruct(struct Student **std, int *capacity, int *count) {
         exit(1);
     }
 }
-
-// Inserting elements in the structure
 void InsertStruct(struct Student **std, int *capacity, int *count, int ID, const char *Name, int Percentage) {
     if (*count == *capacity) {
         *capacity *= 2;
@@ -33,13 +27,11 @@ void InsertStruct(struct Student **std, int *capacity, int *count, int ID, const
     }
     (*std)[*count].ID = ID;
     strncpy((*std)[*count].Name, Name, MAX_TITLE - 1);
-    (*std)[*count].Name[MAX_TITLE - 1] = '\0'; // Ensure null-termination
+    (*std)[*count].Name[MAX_TITLE - 1] = '\0'; 
     (*std)[*count].Percentage = Percentage;
 
     (*count)++;
 }
-
-// Deleting an element from the structure
 void DeleteStruct(struct Student *std, int *count, int ID) {
     int found = 0;
     for (int i = 0; i < *count; i++) {
@@ -56,44 +48,30 @@ void DeleteStruct(struct Student *std, int *count, int ID) {
         printf("The ID isn't present\n");
     }
 }
-
-// Displaying the students
 void displayStudents(struct Student *std, int count) {
     for (int i = 0; i < count; i++) {
         printf("ID: %d, Name: %s, Percentage: %d\n", std[i].ID, std[i].Name, std[i].Percentage);
     }
 }
-
-// Freeing the allocated memory
 void freeStudents(struct Student *std) {
     free(std);
 }
-
 int main() {
     struct Student *std;
     int capacity, count;
     InitStruct(&std, &capacity, &count);
-
     int userid, userpercentage;
     char username[MAX_TITLE];
-
     for (int i = 1; i < 4; i++) {
         printf("What is the ID, Name, and Percentage of the Student: ");
         scanf("%d %s %d", &userid, username, &userpercentage);
         InsertStruct(&std, &capacity, &count, userid, username, userpercentage);
     }
-
     displayStudents(std, count);
-
-    // Example of deleting a student
     printf("Enter the ID of the student to delete: ");
     scanf("%d", &userid);
     DeleteStruct(std, &count, userid);
-
     displayStudents(std, count);
-
-    // Free the allocated memory
     freeStudents(std);
-
     return 0;
 }
