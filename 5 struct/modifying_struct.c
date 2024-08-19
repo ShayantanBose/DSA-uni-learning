cb6e23c64ea13d25786a1639f6df7f7bd8a1dcce
Shayantan_Bose <shnbose13@gmail.com>
2024-08-19 23:52:22 +0530

    changes

---
+++ 5 struct/modifying_struct.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct Student{
+    int ID;
+    char Name[50];
+    float Percentage;
+};
+int main(){
+    struct Student std;
+    struct Student 
+    // for(int i=1;i<4;i++){
+    //     printf("What is the name of the Student:- ");
+    //     scanf("%s",&std[i].Name);
+    //     printf("What is the ID of the Student:- ");
+    //     scanf("%s",&std[i].ID);
+    //     printf("What is the Percentge of the Student:- ");
+    //     scanf("%s",&std[i].Percentage);
+    // }
+    // for(int i=0;i<4;i++){
+    //     printf("The name of the student is %s\n",std[i].Name);
+    //     printf("The id of the student is %d\n",std[i].ID);
+    //     printf("The percentage of the student is %f\n",std[i].Percentage);
+    // }
+
+}
